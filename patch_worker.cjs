845595aec8ba0ffa674b393a75539e86005d3f8a
賴首均 <andrewlai1209@gmail.com>
2026-07-09 15:15:04 +0100

    Revert "Add files via upload"

--- patch_worker.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/workers/search.worker.ts', 'utf8');
-
-code = code.replace(/const result = engine\.search\(fen, trainingProgress\);/g, "const result = await engine.search(fen, trainingProgress);");
-
-fs.writeFileSync('src/workers/search.worker.ts', code);
